62e7b71a8ceb80a55824d8a422ef8fcf9ac0323d
mawenling-mwl <mawenling@gaosiedu.com>
2019-07-05 17:42:43 +0800

    Initial commit

---
+++ src/test/java/com/course/TestDemo.java
@@ -0,0 +1,15 @@
+package com.course;
+
+import org.testng.annotations.Test;
+
+public class TestDemo{
+        @Test
+        public void testcase1(){
+            System.out.println("testcase1");
+        }
+        @Test
+        public void testcase2(){
+            System.out.println("testcase1");
+        }
+}
+
